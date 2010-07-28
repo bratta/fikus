22770f539455aacc28879d470e1d9d889eab6e55
bratta <tgourley@gmail.com>
2010-07-28 17:13:09 -0500

    Fixing mongohq url

--- config/database.rb
+++ config/database.rb
@@ -1,5 +1,5 @@
 if ENV['MONGOHQ_URL']
-  MongoMapper.connection = Mongo::Connection.from_uri(mongodb_uri, :logger => logger)
+  MongoMapper.connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'], :logger => logger)
 else
   MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger)
   MongoMapper.database = "fikus_#{Padrino.env}"
